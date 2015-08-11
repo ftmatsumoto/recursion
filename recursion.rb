ce2b94d36df9ea0880df087a676a4f584379098c
ftmatsumoto <felipetmatsumoto@yahoo.com.br>
2015-08-11 14:57:55 -0300

    merge_sort funcionando

--- recursion.rb
+++ recursion.rb
@@ -18,8 +18,33 @@ def fibs_rec(n)
 end
 
 def merge_sort(ary)
-	
+	len = ary.length
+	return ary if len == 1
+	half = (len/2.0).ceil
+
+	a1 = ary.each_slice(half).to_a.first
+	a2 = ary.each_slice(half).to_a.last
+	b1 = merge_sort(a1)
+	b2 = merge_sort(a2)
+	merge(b1,b2)
 end
 
-puts fibs(20)
-puts fibs_rec(20)
+def merge(ary1, ary2)
+	n = ary1.length + ary2.length
+	res_ary = []
+	n.times do
+		if ary1.empty?
+			res_ary << ary2.shift
+		elsif ary2.empty?
+			res_ary << ary1.shift
+		else
+			ary1[0] < ary2[0] ? res_ary << ary1.shift : res_ary << ary2.shift
+		end
+	end
+	res_ary
+end
+
+print merge_sort([43,2309,439024,2909,40930,9,4242,102,4324,563,6907,82374,87342,0])
+#[43,2309,439024,2909,40930,9,4242,102,4324,563,6907,82374,87342,0]
+#puts fibs(20)
+#puts fibs_rec(20)
